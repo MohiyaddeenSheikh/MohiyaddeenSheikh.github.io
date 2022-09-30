628c657450f1938e4644d8be432e4bd8525b357d
moin <moinsheik@student.iul.ac.in>
2022-10-01 02:40:06 +0530

    small media screen completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,11 +145,36 @@ window.addEventListener('scroll',scrollActive)
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
     const nav = document.getElementById('header');
-    if(this.scrollY >=200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if(this.scrollY >=80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll',scrollHeader) 
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
-
+function scrollUp(){
+    const scrollUp =document.getElementById('scroll-up');
+    if(this.scrollY >=560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+//previously selected topic
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = ()=> document.body.classList.contains(darkTheme)? 'dark':'light'
+const getCurrentIcon = ()=> document.body.classList.contains(iconTheme)? 'uil-moon':'uil-sun'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme==='dark'?'add':'remove'](darkTheme)
+    themeButton.classList[selectedTheme==='uil-moon'?'add':'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click',()=>{
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme',getCurrentTheme())
+    localStorage.setItem('selected-icon',getCurrentIcon())
+})
