67c9bb42858dfcecd627735cf40e19e2dd6a1a43
Mohiyaddeen <83339902+MohiyaddeenSheikh@users.noreply.github.com>
2022-12-26 15:34:36 +0530

    snow js upddated

---
+++ assets/js/snow.js
@@ -0,0 +1,14 @@
+
+
+	window.addEventListener("load", function() {
+		jQuery(document).ready(function($){
+			$(document).christmasify({
+	      snowflakes: 25,
+	      classy_snow: true,
+	      snow_speed: 'medium',
+	      santa: true,
+	      music: false,
+	      image_frame: true,
+	      font: true			}); 
+		});
+	});
